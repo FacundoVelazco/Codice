093938fa52c5af754909ca99b2d25ff21dd8a209
facun <facundovelazco13@gmail.com>
2021-10-17 17:16:55 -0300

    Generando consistencia, problema con node_modules

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const { app, BrowserWindow} = require('electron');
+
 const path = require('path');
+
 function createWindow () {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      // icon: path.join(__dirname,"/imagenes/icono.ico")
-      icon: path.join("C:/Users/facun/Desktop/Sin nombre.ico")
+      show:false,
+      // width: 1366,
+      // height: 768, 
+      frame: true,
+      titleBarStyle: 'hidden',
+      titleBarOverlay: {
+        color: '#2f3241',
+        symbolColor: '#74b1be'
+      },
+      icon: path.join(__dirname,"/imagenes/icono.ico")
+      // icon: path.join("C:/Users/facun/Desktop/Sin nombre.ico")
     })
   
     win.loadFile('index.html')
-  }
+
+    win.once('ready-to-show', () => {
+      win.show();
+    })
+    return win;
+  } 
   app.whenReady().then(() => {
-    createWindow()
+    let win = createWindow();
+
+    win.on('closed', () => {
+      win=null;
+      app.quit();
+    })
   })
   
   
